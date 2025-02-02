86eb1c571a42b0fae2ec0a86530393ad7e73f8b7
Anas Muhammad <anasmuha37@gmail.com>
2025-02-03 03:55:55 +0500

    JS Home Work

---
+++ JS/first.js
@@ -0,0 +1,10 @@
+// Chapter_#1 //
+// Q#1 //
+alert("Error! Please enter a valid password.")
+
+// // Q#3 //
+// let message ="Welcome to JS Land...
+// <break> + 
+// Happy Coding!";
+alert("Welcome to JS Land.")
+alert("Welcome to JS Land.")
