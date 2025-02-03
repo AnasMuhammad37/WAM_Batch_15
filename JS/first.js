bfd6dabfb402a74f3271698e4605f9c6e8fa2aba
Anas Muhammad <anasmuha37@gmail.com>
2025-02-04 02:42:43 +0500

    update

--- JS/first.js
+++ JS/first.js
@@ -1,10 +1,53 @@
 // Chapter_#1 //
 // Q#1 //
-alert("Error! Please enter a valid password.")
+alert("Error! Please enter a valid password.");
 
 // // Q#3 //
 // let message ="Welcome to JS Land...
 // <break> + 
 // Happy Coding!";
-alert("Welcome to JS Land.")
-alert("Welcome to JS Land.")
+alert("Welcome to JS Land.");
+alert("Welcome to JS Land.");
+
+// Chapter_#2 //
+// Q#1 //
+var username = "Anas";
+alert(username)
+
+var name = "Anas Muhammad";
+alert(name)
+
+// Q#2 //
+var age= "21";
+alert(age)
+
+// Q#3 //
+var message;
+message = "Hellow World";
+// alert(message);
+
+ // Q#4 //
+var course = "Certified Mobile Application Development";
+alert(course);
+
+// Q#5 //
+// let message = "PIZZA\nPIZZ\nPIZ\nPI\nP";  
+// alert(message);
+
+ // Q#6 //
+var email = "anasmuha37@gmail.com";
+var message = "my eamil addres is " + email;
+alert(message);
+
+ // Q#7 //
+let book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+// Chapter_#3 //
+// Q#1 //
+var message = "I am 15 years old ";
+alert(message);
+// Q#3 //
+// document.write("My birth year is " + birthYear + "<br>");
+// document.write("Data type of my declared variable is " + typeof birthYear);
+// var birthYear = 1990;
